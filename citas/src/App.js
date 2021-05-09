25a706ced06430f65b3355cd2a2c4ffe292eee91
= <=>
2021-05-08 20:36:54 -0500

    Validación

--- citas/src/App.js
+++ citas/src/App.js
@@ -6,15 +6,17 @@ function App() {
   return (
     <Fragment>
       <h1>Administrador de pacientes</h1>
-      {/* Estas definiciones de nombres de clase se hacen para el framework css skeleton */}
+      {/* Estas definiciones de nombres de clase se hacen para el framework css skeleton 
+        que se pusieron en las etiquetas link del archivo index.html de la carpeta public*/}
       <div className="container">
         <div className="row">
-          <div className="one-half column"><Formulario /></div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+            {/* Este es mi componente Formulario */}
+            <Formulario />
+          </div>
+          <div className="one-half column">Segundo Div</div>
         </div>
       </div>
-
-
     </Fragment>
   );
 }
--- citas/src/components/Formulario.js
+++ citas/src/components/Formulario.js
@@ -11,24 +11,55 @@ const Formulario = () => {
         sintomas:''
     })
 
+    //Segundo state para el manejo de mensajes de error
+    const [ error, actualizarError ] = useState(false)
+
     //Funcion que se ejecuta cuando el usuario escribe en un inpt
+    //Si es un solo parametro no necesita los paraentesis
     const actualizarState = evento => {
-        //Aqui se debe modificar la cita, que es la "cosa" que se declara en ela linea 6
+        //Aqui se debe modificar la cita, que es la "cosa" que se declara en la linea 6
         //console.log(evento.target.name)
         //console.log([evento.target.name],evento.target.value)
         actualizarCita({
             ...cita,//Toma una copia y con la siguiente linea se reescribe solo el elemento que se pasa
             //Aqui se usará array destructuring, para que la asignación sea dinamica
+            //"mascota":"Lo que se escriba en el input mascota"
             [evento.target.name]:evento.target.value
         })
     }
 
+    //Extraer los valores, destructuring, para poder usar los elementos individualmente
     const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+    //Cuando el usuario presiona el botón Agregar Cita o envía el formulario
+    const submitCita = (eventoBoton) => {
+        //Por defecto se envian los datos por la URL
+        //Entonces se pone esto para que no lo haga
+        eventoBoton.preventDefault();
+
+        //Validar
+        if(mascota.trim()==='' || propietario.trim()==='' || fecha.trim()==='' || hora.trim()==='' || sintomas.trim()===''){
+            //console.log('Todos los campos deben ser diligenciados');
+            actualizarError(true)
+            //Este return hace que se salga y no siga ejecutando codigo
+            return;
+        }
+
+        //Asignar un ID
+
+        //Crear la cita, colocarla en el statePrincipal
+
+        //Reiniciar el form
+
+    }
 
     return ( 
         <Fragment>
             <h2>Crear Cita</h2>
-            <form>
+            { error ? <p className='alerta-error'>Todos los campos son obligatorios.</p> : null }
+            <form
+                onSubmit={submitCita}
+            >
                 <label>Nombre Mascota</label>
                 <input
                     type="text"
